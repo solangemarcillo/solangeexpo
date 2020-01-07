867980a665957389b91c3be05b170c33f5621f1e
marcillo villafuerte solange virginia <marcillovillafuertesolange@gmail.com>
2020-01-07 17:05:04 -0500

    Agregar archivos de proyecto.

---
+++ expo1/LucesLargas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace expo1
+{
+	class LucesLargas:LucesReceiver
+	{
+		private const int DISTANCIA = 200;
+
+		public override int Encender()
+		{
+			this.encendidas = true;
+			return DISTANCIA;
+		}
+	}
+}
---
+++ expo1/LucesReceiver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace expo1
+{
+	class LucesReceiver
+	{
+		protected bool encendidas;
+		protected int distanciaAlumbrado;
+
+		// Propiedad de sólo lectura que devolverá el estado de las luces
+		public bool Encendidas
+		{
+			get { return encendidas; }
+		}
+
+		// Método encargado de apagar las luces. Establece el estado del atributo 'encendidas'
+		// a 'false'. Será común a todas las clases que hereden de ella.
+		public void Apagar()
+		{
+			this.encendidas = false;
+		}
+
+		// El método Encender será distinto en cada una de las clases que hereden de esta clase.
+		public abstract int Encender();
+	
+    }
+}
